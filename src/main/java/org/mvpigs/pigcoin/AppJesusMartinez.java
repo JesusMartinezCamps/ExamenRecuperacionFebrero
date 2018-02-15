4e84a56a267504570d92853031622b1d736f5047
Jesús Martínez <gsus.martinez97@gmail.com>
2018-02-15 17:22:19 +0100

    Constructores de Transaction añadidos

--- src/main/java/org/mvpigs/pigcoin/AppJesusMartinez.java
+++ src/main/java/org/mvpigs/pigcoin/AppJesusMartinez.java
@@ -54,7 +54,7 @@ public class AppJesusMartinez {
                                   "==============="        );
 
         Transaction trx = new Transaction();
-       // trx = new Transaction("hash_1", "0", wallet_1.getAddress(), wallet_2.getAddress(), 20, "a flying pig!");
+        trx = new Transaction("hash_1", "0", wallet_1.getAddress(), wallet_2.getAddress(), 20, "a flying pig!");
         
         System.out.println(trx.toString());
 
--- src/main/java/org/mvpigs/pigcoin/Transaction.java
+++ src/main/java/org/mvpigs/pigcoin/Transaction.java
@@ -1,5 +1,28 @@
 package org.mvpigs.pigcoin;
 
+import java.security.PublicKey;
+
 public class Transaction {
 
+	//attributes
+	private String hash = "";
+	private String prev_hash = "";
+	private PublicKey pKey_sender = null;
+	private PublicKey pKey_recipient = null;
+	private int pigcoins = 0;
+	private String message = "";
+	
+	
+	//constructor
+	public Transaction() {
+		
+	}
+	public Transaction(String hash, String prev_Hash, PublicKey pKey_sender, PublicKey pKey_recipient, int pigcoins, String message) {
+		this.hash = hash;
+		this.prev_hash = prev_hash;
+		this.pKey_sender = pKey_sender;
+		this.pKey_recipient = pKey_recipient;
+		this.pigcoins = pigcoins;
+		this.message = message;
+	}
 }

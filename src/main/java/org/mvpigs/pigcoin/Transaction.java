65c78d7b6674a92d509d4c25a4e00aba01a7ed55
Jesús Martínez <gsus.martinez97@gmail.com>
2018-02-15 17:32:31 +0100

    Getters básicos clase transaction y casos test de constructor añadidos

--- src/main/java/org/mvpigs/pigcoin/Transaction.java
+++ src/main/java/org/mvpigs/pigcoin/Transaction.java
@@ -17,12 +17,32 @@ public class Transaction {
 	public Transaction() {
 		
 	}
-	public Transaction(String hash, String prev_Hash, PublicKey pKey_sender, PublicKey pKey_recipient, int pigcoins, String message) {
+	public Transaction(String hash, String prev_hash, PublicKey pKey_sender, PublicKey pKey_recipient, int pigcoins, String message) {
 		this.hash = hash;
 		this.prev_hash = prev_hash;
 		this.pKey_sender = pKey_sender;
 		this.pKey_recipient = pKey_recipient;
 		this.pigcoins = pigcoins;
 		this.message = message;
+	}
+	
+	//getters & setters
+	public String getHash() {
+		return this.hash;
+	}
+	public String getPrevHash() {
+		return this.prev_hash;
+	}
+	public PublicKey getpKeySender() {
+		return this.pKey_sender;
+	}
+	public PublicKey getpKeyRecipient() {
+		return this.pKey_recipient;
+	}
+	public int getPigcoins() {
+		return this.pigcoins;
+	}
+	public String getMessage() {
+		return this.message;
 	}
 }
---
+++ src/test/java/org/mvpigs/pigcoin/tests/TransactionTest.java
@@ -0,0 +1,42 @@
+package org.mvpigs.pigcoin.tests;
+
+import static org.junit.Assert.*;
+
+import java.security.PublicKey;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mvpigs.pigcoin.Transaction;
+import org.mvpigs.pigcoin.Wallet;
+
+public class TransactionTest {
+
+	Wallet wallet;
+	Wallet wallet2;
+	@Before
+	public void setUp() {
+		wallet = new Wallet();
+		wallet2 = new Wallet();
+	}
+	
+	@Test
+	public void constructorTest() {
+		String hash1 = "hash_1";
+		String hash2 = "0";
+		PublicKey pKey = wallet.getAddress();
+		PublicKey pKey2 = wallet2.getAddress();
+		int pigcoins = 20;
+		String message = "a flying pig!";
+		
+		Transaction trx = new Transaction(hash1, hash2, pKey, pKey2, pigcoins, message);
+		
+		assertEquals(hash1, trx.getHash());
+		assertEquals(hash2, trx.getPrevHash());
+		assertEquals(pKey, trx.getpKeyRecipient());
+		assertEquals(pKey2, trx.getpKeySender());
+		assertEquals(pigcoins, trx.getPigcoins());
+		assertEquals(message, trx.getMessage());
+		
+	}
+
+}
